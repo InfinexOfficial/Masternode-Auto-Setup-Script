fd3a82b81a20fd8d69e2cefa532eeadbc7e4643b
Infinex <33971094+InfinexOfficial@users.noreply.github.com>
2018-02-11 12:53:48 +0800

    Update infinexscript.sh

--- infinexscript.sh
+++ infinexscript.sh
@@ -15,6 +15,7 @@ sudo apt-get dist-upgrade -y
 sudo apt-get install nano htop git -y
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common -y
 sudo apt-get install libboost-all-dev -y
+sudo apt-get install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
