8ccdb78b2a03a76a1c32d55fcf16d95f8ea69404
Infinex <33971094+InfinexOfficial@users.noreply.github.com>
2018-03-06 21:31:29 +0800

    Create basesetupscript.sh

---
+++ basesetupscript.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+if free | awk '/^Swap:/ {exit !$2}'; then
+echo "Have swap"
+else
+sudo touch /var/swap.img
+sudo chmod 600 /var/swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+fi
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install nano htop git -y
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common -y
+sudo apt-get install libboost-all-dev -y
+sudo apt-get install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
