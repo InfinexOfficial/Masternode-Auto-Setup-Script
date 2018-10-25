94e6dcf9ef93476da6632a709b64c1b44a51c354
Infinex <33971094+InfinexOfficial@users.noreply.github.com>
2018-10-25 17:53:54 +0800

    Update basesetupscript.sh

--- basesetupscript.sh
+++ basesetupscript.sh
@@ -5,7 +5,7 @@ echo "Have swap"
 else
 sudo touch /var/swap.img
 sudo chmod 600 /var/swap.img
-sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=4000
 mkswap /var/swap.img
 sudo swapon /var/swap.img
 sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
